fdde29360a22d7bca92f0d98d3fed42f4ea6562b
uniquepatel01 <uniquepatel01@gmail.com>
2024-12-21 11:01:55 +0530

    Adding some Animations

--- src/components/Hero-section/Hero.jsx
+++ src/components/Hero-section/Hero.jsx
@@ -1,11 +1,15 @@
-import {motion} from "framer-motion";
+import {delay, motion} from "framer-motion";
 import { FaArrowRight } from "react-icons/fa6";
 import hero from "../Hero-section/home-page.png"
 
 export default function Hero() {
   return (
     <>
-      <div className="w-[550px] mx-52 my-44  top-[270px] left-[200px]">
+      <motion.div className="w-[550px] mx-52 my-44  top-[270px] left-[200px]"
+                  initial={{x:-100, opacity:0}}
+                  animate={{x:0, opacity:1}}
+                  transition={{ ease: "easeOut", duration: 1 , delay:0.1 }}
+      >
         <h1 className="text-6xl font-custom text-gray-600 font-semibold">
           Your Favourite Food Delivered Hot &
           Fresh
@@ -15,8 +19,13 @@ export default function Hero() {
           marinating, so you can cook <br /> a fresh food.
         </p>
         <button className="h-[52px] w-[170px] bg-yellow-500 text-white border rounded-3xl flex items-center justify-center gap-3 text-lg font-semibold">Order Now <FaArrowRight/></button>
-      </div>
-        <img className="w-[700px] absolute right-40 top-16" src={hero} alt="" />
+      </motion.div>
+        <motion.img className="w-[700px] absolute right-40 top-16" src={hero} alt=""
+          initial={{x:100, opacity:0}}
+          animate={{x:0, opacity:1}}
+          transition={{ ease: "easeOut", duration: 1 , delay:0.1 }}
+          
+        />
     </>
   );
 }
